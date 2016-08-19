9983471c2fc5ed2708bc2ec75284a725926b2457
Felipe Martinez Amaral <amaral1@motorola.com>
2016-08-19 11:27:20 -0300

    Add new servlet to test ORM

---
+++ src/main/java/com/ciandt/poc/BigTableORMServlet.java
@@ -0,0 +1,67 @@
+package com.ciandt.poc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ciandt.poc.entities.AppDevice;
+import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.appengine.repackaged.com.google.gson.internal.LinkedTreeMap;
+import com.google.common.reflect.TypeToken;
+
+/**
+ * Created by famaral on 8/15/16.
+ */
+@WebServlet(name = "orm", urlPatterns = {"/orm/appdevice"} )
+public class BigTableORMServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 6253116304248759593L;
+
+	/**
+	 * Servlet responsible to get a table
+	 * curl 'http://localhost:8080/orm/appdevice?rowkey=1234' \
+	 */
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		if(req.getRequestURI().equals("/favicon.ico"))
+			return;
+		String pathInfo = req.getPathInfo();
+		String rowkey = pathInfo.split("/")[1];
+		resp.setContentType("text/plain");
+		PrintWriter pw = resp.getWriter();
+		pw.println("Row found: \n  " + new BigtableORMHelper<AppDevice>(AppDevice.class).getRowByKey(rowkey));
+		pw.close();
+	}
+
+	/**
+	 *  Servlet responsible to create a new table on BigTable
+	 *  usage:
+	 *  curl 'http://localhost:8080/bigtable?tablename=mytable' \
+	 *  -X POST -H 'Content-Type: application/json' \
+	 *   --data-binary $"{'row_key':'mykey','field1':'value1'}"
+	 */
+	@SuppressWarnings("serial")
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		if(req.getRequestURI().equals("/favicon.ico"))
+			return;
+		String tableName = req.getParameter("tablename");
+		String body = req.getReader().lines()
+				.reduce("", (accumulator, actual) -> accumulator + actual);
+		LinkedTreeMap<String, Object> list = new Gson().fromJson(
+				body, new TypeToken<LinkedTreeMap<String, Object>>() {}.getType());
+		resp.setContentType("text/plain");
+		PrintWriter pw = resp.getWriter();
+		AppDevice appDevice = new AppDevice();
+		//TODO Add fields from appdevice post body
+		pw.println("Table created in " + new BigtableORMHelper<AppDevice>(AppDevice.class).insert(appDevice));
+		pw.close();
+	}
+}
